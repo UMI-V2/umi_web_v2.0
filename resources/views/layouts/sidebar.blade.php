5f52392acd704aa611ba0c25caa4817ac77e49b4
yorizpra <yogarizkipratama@gmail.com>
2022-02-20 14:45:15 +0700

    menambahkan logo, user pic, dan favicon

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ url('/home') }}" class="brand-link">
-        <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
+        <!-- <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
              alt="{{ config('app.name') }} Logo"
+             class="brand-image img-circle elevation-3"> -->
+             <img src="{{url('/images/logo_umi.jpg')}}" alt="{{ config('app.name') }} Logo"
              class="brand-image img-circle elevation-3">
         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
     </a>
