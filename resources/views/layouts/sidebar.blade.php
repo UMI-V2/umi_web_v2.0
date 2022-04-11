0b5d3867f3f96845d40d3eed2ae104517a70857e
yorizpra <yogarizkipratama@gmail.com>
2022-04-11 11:12:31 +0700

    mengubah layout sidebar, body dan navbar menjadi dark mode

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-light-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ url('/dashboard') }}" class="brand-link">
         <!-- <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
              alt="{{ config('app.name') }} Logo"
