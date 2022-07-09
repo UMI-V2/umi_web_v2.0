a803104609efc3bdbcaa76bf51e7fdacf4704d11
Abdan009 <mabdanmaruf@gmail.com>
2022-07-09 15:07:37 +0700

    Add Address Delivery API

---
+++ database/migrations/2022_07_09_143648_create_address_deliveries_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressDeliveriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address_deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_transaksi_penjualan');
+            $table->string('nama');
+            $table->string('no_hp');
+            $table->string('alamat_lengkap');
+            $table->string('patokan');
+            $table->boolean('is_alamat_utama')->default(false);
+            $table->boolean('is_rumah')->default(false);
+            $table->boolean('is_kantor')->default(false);
+            $table->boolean('is_usaha')->default(false);
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('id_transaksi_penjualan')->references('id')->on('sales_transactions');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address_deliveries');
+    }
+}
