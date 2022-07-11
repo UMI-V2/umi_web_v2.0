9cf38be87b7709823999c18b47d5956101f9d772
yorizpra <yogarizkipratama@gmail.com>
2022-07-11 21:17:35 +0700

    add jumlah transaksi dan fix conflict di master_privilege

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -94,6 +94,18 @@
                         </div>
 
                     </div>
+                    
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="info-box mb-3">
+                            <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Total Transaksi</span>
+                                <span class="info-box-number">{{ $totalTransaksi }}</span>
+                            </div>
+
+                        </div>
+
+                    </div>
 
                 </div>
 
